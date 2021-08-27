9a1845d9656bbea3c2caaa40722c0a415d7bd85d
OpenDevloper <89633504+OpenDevloper@users.noreply.github.com>
2021-08-27 13:55:56 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,124 @@
+
+from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+
+def newFile():
+    global file
+    root.title("Untitled - Notepad")
+    file = None
+    TextArea.delete(1.0, END)
+
+
+def openFile():
+    global file
+    file = askopenfilename(defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+    if file == "":
+        file = None
+    else:
+        root.title(os.path.basename(file) + " - Notepad")
+        TextArea.delete(1.0, END)
+        f = open(file, "r")
+        TextArea.insert(1.0, f.read())
+        f.close()
+
+
+def saveFile():
+    global file
+    if file == None:
+        file = asksaveasfilename(initialfile = 'LinuSample.txt', defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+        if file =="":
+            file = None
+
+        else:
+            #Save as a new file
+            f = open(file, "")
+            f.write(TextArea.get(1.0, END))
+            f.close()
+
+            root.title(os.path.basename(file) + " - Notepad")
+            print("File Saved")
+    else:
+        # Save the file
+        f = open(file, "w")
+        f.write(TextArea.get(1.0, END))
+        f.close()
+
+
+def quitApp():
+    root.destroy()
+
+def cut():
+    TextArea.event_generate(("<>"))
+
+def copy():
+    TextArea.event_generate(("<>"))
+
+def paste():
+    TextArea.event_generate(("<>"))
+
+def about():
+    showinfo("LinuNote", "Copyright SLinuxDevlopment2021, LinuNote is a basic text editor for unix and unix-like operating systems, it is made in python and can be compiled to a .exe, .deb etc. if you want to use LinuNot then you need to compile it using python3")
+
+if __name__ == '__main__':
+    #Basic tkinter setup
+    root = Tk()
+    root.title("LinuxSample.txt - LinuNote")
+    root.geometry("644x788")
+
+    #Add TextArea
+    TextArea = Text(root, font="Monospace 13")
+    file = None
+    TextArea.pack(expand=True, fill=BOTH)
+
+    # Lets create a menubar
+    MenuBar = Menu(root)
+
+    #File Menu Starts
+    FileMenu = Menu(MenuBar, tearoff=0)
+    # To open new file
+    FileMenu.add_command(label="New", command=newFile)
+
+    #To Open already existing file
+    FileMenu.add_command(label="Open", command = openFile)
+
+    # To save the current file
+
+    FileMenu.add_command(label = "Save", command = saveFile)
+    FileMenu.add_separator()
+    FileMenu.add_command(label = "Exit", command = quitApp)
+    MenuBar.add_cascade(label = "File", menu=FileMenu)
+    # File Menu ends
+
+    # Edit Menu Starts
+    EditMenu = Menu(MenuBar, tearoff=0)
+    #To give a feature of cut, copy and paste
+    EditMenu.add_command(label = "Cut", command=cut)
+    EditMenu.add_command(label = "Copy", command=copy)
+    EditMenu.add_command(label = "Paste", command=paste)
+
+    MenuBar.add_cascade(label="Edit", menu = EditMenu)
+
+    # Edit Menu Ends
+
+    # Help Menu Starts
+    HelpMenu = Menu(MenuBar, tearoff=0)
+    HelpMenu.add_command(label = "About LinuNote", command=about)
+    MenuBar.add_cascade(label="About", menu=HelpMenu)
+
+    # Help Menu Ends
+
+    root.config(menu=MenuBar)
+
+    #Adding Scrollbar using rules from Tkinter lecture no 22
+    Scroll = Scrollbar(TextArea)
+    Scroll.pack(side=RIGHT,  fill=Y)
+    Scroll.config(command=TextArea.yview)
+    TextArea.config(yscrollcommand=Scroll.set)
+
+    root.mainloop()
